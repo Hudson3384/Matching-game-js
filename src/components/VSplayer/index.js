5d128c25cfd35b21b744a62e35f58dd6dfd5691b
hudson3384 <hudson.arruda@outlook.com>
2022-07-14 09:04:21 -0300

    :art: Structure scoreBoard Object

---
+++ src/components/VSplayer/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+function VSplayer (content = "vs") {
+    return `
+        <span class = "vs-player">${content}</span>
+        
+    `
+}
+
+export default VSplayer;
---
+++ src/components/scorePlayer/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+function scorePlayer() {
+    return `
+        <section class="score-Player">
+            <article class="score-Point"></article>
+            <article class="score-Point"></article>
+            <article class="score-Point"></article>
+        </section>
+    ` 
+
+}
+
+export default scorePlayer;
